4b247c2e419f9e06db8354f3ea37e5d306ab9d6a
Dominguezd01 <migueldominguezroman01@gmail.com>
2023-05-06 10:53:27 +0200

    .

--- routes/Users/register.routes.js
+++ routes/Users/register.routes.js
@@ -9,37 +9,44 @@ const routerRegister = express.Router()
 routerRegister.post("/users/register", async(req, res) =>{
     const userData = await req.body
     const salt = await bcrypt.genSalt(10)
-    if(userData){
-        if(!userData.name || !userData.email || !userData.password){
-            return res.status(400).json({status:400, message: "Something went wrong with that :C"})
-        }
-
-        const emailAlreadyInUse = await prisma.users.findUnique({where: {email: userData.email}})
-
-        if(emailAlreadyInUse){
-            return res.status(400).json({status: 400, message:"Email already in use"})
-        }
-        const userCreated = await prisma.users.create({
-            data:{
-                id:  String(await generateUUID()),
-                name: userData.name,
-                email: userData.email,
-                profile_picture: userData.profile_picture 
-                                    ? userData.profile_picture 
-                                    : null,
-                password: await bcrypt.hash(userData.password, salt)
+    try{
+        if(userData){
+            if(!userData.name || !userData.email || !userData.password){
+                return res.status(400).json({status:400, message: "Something went wrong with that :C"})
             }
-        })
-
-        if(userCreated){    
-            return res.status(200).json({status:200,message: "Welcome aboard you pretty human :D"})
-        }else{
-            return res.status(500).json({ status:500,message: "Something went really wrong but dotn worry its our fault :C"})
-        }
-         
-    }
-    return res.status(400).json({status:400,message: "Something is not working as intended, my bad :P"})
     
+            const emailAlreadyInUse = await prisma.users.findUnique({where: {email: userData.email}})
+    
+            if(emailAlreadyInUse){
+                return res.status(400).json({status: 400, message:"Email already in use"})
+            }
+            
+            const userCreated = await prisma.users.create({
+                data:{
+                    id:  String(await generateUUID()),
+                    name: userData.name,
+                    email: userData.email,
+                    profile_picture: userData.profile_picture 
+                                        ? userData.profile_picture 
+                                        : null,
+                    password: await bcrypt.hash(userData.password, salt)
+                }
+            })
+    
+            if(userCreated){    
+                return res.status(200).json({status:200,message: "Welcome aboard you pretty human :D"})
+            }else{
+                return res.status(500).json({ status:500,message: "Something went really wrong but dotn worry its our fault :C"})
+            }
+             
+
+    }else{
+        return res.status(400).json({status:400,message: "Something is not working as intended, my bad :P"})
+    
+    }
+    }catch(e){
+        return res.status(500).json({ status:500,message: "Something went really wrong but dotn worry its our fault :C"})
+    }   
 
     
 })
