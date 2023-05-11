f06a30ed8eab65a0343a87f79e67397999818661
Miguel Domínguez <65074687+Dominguezd01@users.noreply.github.com>
2023-05-11 09:36:23 +0000

    .

--- routes/Users/getUserInfo.routes.js
+++ routes/Users/getUserInfo.routes.js
@@ -6,35 +6,33 @@ const getUserInfo = express.Router()
 
 
 getUserInfo.post("/users/getUserInfo", async (req, res) =>{
-    const userData = await req.body
 
-    if(!userData || !userData.password || !userData.userId){
-        return res.status(400).json({status: 400, message: "Something went wrong"})
-    }
-
-    const userExists = await prisma.users.findUnique({ where: { email: userData.email } })
+    try{
+        const userData = await req.body
+        if(!userData || !userData.userId){
+            return res.status(400).json({status: 400, message: "Something went wrong"})
+        }
     
-    if (!userExists) {
-        return res.status(400).json({ status: 400, message: "Wrong email or password" })
+        const userExists = await prisma.users.findUnique({ where: { email: userData.email } })
+        
+        if (!userExists) {
+            return res.status(400).json({ status: 400, message: "Wrong email or password" })
+        }
+    
+        const sendData = {
+            name: userExists.name,
+            id: userExists.id,
+            email: userExists.email,
+            proffilePicture: userExists.profile_picture
+        }
+    
+       return res.status(200).json({status: 200, sendData, message: "Good to see you again :D" })
+    
+    }catch(e){
+        return res.status(500).json({status: 500, message: "Something went wrong in our servers" })
     }
 
-    bcrypt.compare(userData.password, userExists.password, (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: "Something went wrong" })
-        }
-        if (result) {
-            const sendData = {
-                name: userExists.name,
-                id: userExists.id,
-                email: userExists.email,
-                proffilePicture: userExists.profile_picture
-            }
 
-           return res.status(200).json({status: 200, sendData, message: "Good to see you again :D" })
-        }
-    })
-
-    return res.status(200).json({status: 200})
 })
 
 export default getUserInfo
